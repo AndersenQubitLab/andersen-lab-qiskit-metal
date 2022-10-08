efe74b15c468cb43344ec7649766287d7f81948d
Figen Yilmaz <f.yilmaz@tudelft.nl>
2022-10-08 12:38:10 +0200

    Update ebeam_marker.py

--- qiskit_metal/qlibrary/terminations/ebeam_marker.py
+++ qiskit_metal/qlibrary/terminations/ebeam_marker.py
@@ -29,26 +29,15 @@ from qiskit_metal.qlibrary.core import QComponent
 
 
 class Markers(QComponent):
-    """Launch pad to feed/read signals to/from the chip.
+    """ * to/from the chip.
 
     Inherits 'QComponent' class.
 
     .. image:
-        LaunchpadWirebondCoupled.png
+        Frame.png
 
     .. meta::
-        Launchpad Wirebond Coupled
-
-    Creates a 50 ohm launch pad with a ground pocket cutout.
-    Limited but expandable parameters to control the launchpad polygons.
-    The (0,0) point is the center of the necking of the launch tip.
-    The pin attaches directly to the built in lead length at its midpoint
-    This launch has an inductive coupler section.
-
-    Pocket and pad:
-        Pocket and launch pad geometries are currently fixed.
-        (0,0) point is the midpoint of the necking of the launch tip.
-        Pocket is a negative shape that is cut out of the ground plane
+        Frame
 
     Values (unless noted) are strings with units included, (e.g., '30um')
 
