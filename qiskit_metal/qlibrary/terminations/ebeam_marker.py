cf8d6bf9887180845d2adbc146d3856b25aaf5b0
Figen Yilmaz <f.yilmaz@tudelft.nl>
2022-10-12 16:24:59 +0200

    Update ebeam_marker.py

--- qiskit_metal/qlibrary/terminations/ebeam_marker.py
+++ qiskit_metal/qlibrary/terminations/ebeam_marker.py
@@ -29,15 +29,26 @@ from qiskit_metal.qlibrary.core import QComponent
 
 
 class Markers(QComponent):
-    """ * to/from the chip.
+    """Launch pad to feed/read signals to/from the chip.
 
     Inherits 'QComponent' class.
 
     .. image:
-        Frame.png
+        *.png
 
     .. meta::
-        Frame
+        *
+
+    Creates a 50 ohm launch pad with a ground pocket cutout.
+    Limited but expandable parameters to control the launchpad polygons.
+    The (0,0) point is the center of the necking of the launch tip.
+    The pin attaches directly to the built in lead length at its midpoint
+    This launch has an inductive coupler section.
+
+    Pocket and pad:
+        Pocket and launch pad geometries are currently fixed.
+        (0,0) point is the midpoint of the necking of the launch tip.
+        Pocket is a negative shape that is cut out of the ground plane
 
     Values (unless noted) are strings with units included, (e.g., '30um')
 
@@ -56,20 +67,23 @@ class Markers(QComponent):
         Markers.png
 
     Default Options:
-        * marker: '20um' -- center trace width of the marker
+        * pos_x = '0.0mm' -- the x position of the marker
+        * pos_y = '0.0mm' -- the y position of the marker
+        * marker_sep = '20um' -- the distance between each marker
+        * marker_w='20um' -- marker width
+        * marker_h='20um' -- marker height
     """
 
     default_options = Dict(
-        chip='main',
-        pos_x = '0.0 mm',
-        pos_y = '0.0 mm',
-        marker_sep = '30um',
+        pos_x = '0.0mm',
+        pos_y = '0.0mm',
+        marker_sep = '20um',
         marker_w='20um',
         marker_h='20um',
          )
     """Default options"""
 
-    TOOLTIP = """Markers for the chip."""
+    TOOLTIP = """Markers for the chip"""
 
     def make(self):
         """This is executed by the user to generate the qgeometry for the
@@ -82,24 +96,24 @@ class Markers(QComponent):
         pos_y = p.pos_y
         marker_sep = p.marker_sep
 
-        # Square shape markers for E-Beam marker search, check ...
-        marker_1 = draw.rectangle(marker_w, marker_h, pos_x, pos_y)
-        marker_2 = draw.rectangle(marker_w, marker_h, pos_x - marker_sep, pos_y)
-        marker_3 = draw.rectangle(marker_w, marker_h, pos_x - marker_sep, pos_y - marker_sep)
-        marker_4 = draw.rectangle(marker_w, marker_h, pos_x, pos_y - marker_sep)
+        # Square shape markers for E-Beam marker search, see the drawing above for their positions
+        # on the cartesian coordinate system
+        marker_1 = draw.rectangle(marker_w, marker_h, pos_x/2 + marker_sep, pos_y/2 + marker_sep)
+        marker_2 = draw.rectangle(marker_w, marker_h, pos_x/2 - marker_sep, pos_y/2 + marker_sep)
+        marker_3 = draw.rectangle(marker_w, marker_h, pos_x/2 + marker_sep, pos_y/2 - marker_sep)
+        marker_4 = draw.rectangle(marker_w, marker_h, pos_x/2 - marker_sep, pos_y/2 - marker_sep)
 
-        marker = draw.union(marker_1, marker_2, marker_3, marker_4)
+        markers = draw.union(marker_1, marker_2, marker_3, marker_4)
 
         # Create polygon object list
-        polys = [marker]
-
+        polys = [markers]
         # Rotates and translates all the objects as requested. Uses package functions
         # in 'draw_utility' for easy rotation/translation
         polys = draw.rotate(polys, p.orientation, origin=(0, 0))
-        polys = draw.translate(polys, xoff=p.pos_x, yoff=p.pos_y)
-        [marker] = polys
+        polys = draw.translate(polys, xoff=pos_x/2, yoff=pos_y/2)
+        [markers] = polys
 
         # Adds the object to the qgeometry table
         self.add_qgeometry('poly',
-                           dict(marker=marker),
+                           dict(markers=markers),
                            layer=p.layer)
